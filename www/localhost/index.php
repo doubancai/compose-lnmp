bcb523b7cb9feb6e2278e6a5538c8bf97763bc2e
xx <xx@example.com>
2022-08-18 17:53:03 +0800

    Redis连接测试

--- www/localhost/index.php
+++ www/localhost/index.php
@@ -6,3 +6,16 @@ $user = 'root';
 $password = 'root';
 $pdo = new PDO($dsn,$user,$password);
 var_dump($pdo);
+
+echo "<p>";
+$redis = new Redis();
+$redis->connect('redis62', 6379);
+$redis->auth('auth123');
+echo "Server is running: " . $redis->ping();
+
+echo "<p>";
+$redis = new Redis();
+$redis->connect('redis70', 6379);
+$redis->auth('auth123');
+echo "Server is running: " . $redis->ping();
+
