7005f3a25a3d6109a57b8b295d4b0bdbd319578e
xx <xx@example.com>
2022-08-18 17:57:31 +0800

    安装MongoDB50

--- README.md
+++ README.md
@@ -144,5 +144,35 @@ docker-compose up
 
 127.0.0.1
 
+## MongoDB50
+
+1.拷贝容器内配置文件到宿主机
+
+```bash
+docker run -it --rm --name temp-mongo mongo:5.0.10 /bin/bash
+mkdir ./services/mongo50/
+docker cp temp-mongo:/etc/mongod.conf.orig ./services/mongo50/
+```
+
+2.启动容器
+
+```bash
+docker-compose up
+```
+
+3.浏览器中访问
+
+127.0.0.1
+
+4.设置密码
+
+报错 The SCRAM_SHA_1 authentication mechanism requires libmongoc built with ENABLE_SSL
+
+需要php安装ssl
+
+报错Uncaught PHP Exception MongoDB\Driver\Exception\AuthenticationException: “Authentication failed.”
+
+需要将data/mongo目录清空重启mongo容器
+
 
 
--- www/localhost/index.php
+++ www/localhost/index.php
@@ -19,3 +19,26 @@ $redis->connect('redis70', 6379);
 $redis->auth('auth123');
 echo "Server is running: " . $redis->ping();
 
+echo "<p>";
+$manager = new MongoDB\Driver\Manager("mongodb://admin:auth123@mongo50:27017");
+
+// 插入数据
+$bulk = new MongoDB\Driver\BulkWrite;
+$bulk->insert(['x' => 1, 'name'=>'测试1', 'title' => 'test1']);
+$bulk->insert(['x' => 2, 'name'=>'测试2', 'title' => 'test2']);
+$manager->executeBulkWrite('test.sites', $bulk);
+
+$filter = ['x' => ['$gt' => 0]];
+$options = [
+    'projection' => ['_id' => 0],
+    'sort' => ['x' => -1],
+];
+
+// 查询数据
+$query = new MongoDB\Driver\Query($filter, $options);
+$cursor = $manager->executeQuery('test.sites', $query);
+foreach ($cursor as $document) {
+    print_r($document);
+}
+
+
